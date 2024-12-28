0c008aa336863c2e192a3fcfc87952aa8d4a6019
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-12-28 13:05:37 +0700

    UI Home Page

---
+++ src/components/common/HeaderComp.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <header>
+    <nav>
+      <div>
+        <h1>SHOP.CO</h1>
+      </div>
+      <div class="">
+        <RouterLink to="/"> Shop </RouterLink>
+        <RouterLink to="/"> On Sale </RouterLink>
+        <RouterLink to="/"> New Arrival </RouterLink>
+        <RouterLink to="/"> Brands </RouterLink>
+      </div>
+      <div class="">
+        <input class="bg-slate-200 rounded-full py-1" placeholder="Cari barang" type="search">
+      </div>
+    </nav>
+  </header>
+</template>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import HeaderComp from "@/components/common/HeaderComp.vue";
+import { RouterView } from "vue-router";
+</script>
+<template>
+  <div class="">
+    <HeaderComp />
+    <main>
+      <RouterView />
+    </main>
+  </div>
+</template>
